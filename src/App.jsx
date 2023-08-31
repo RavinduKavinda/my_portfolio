ef376c484bf5b142b602e555ecdbdbc5fbd17397
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-08-31 18:22:08 +0530

    setup project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Project from './pages/project/Project'
+import Contact from './pages/contact/Contact'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
