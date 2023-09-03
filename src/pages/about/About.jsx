382ff4794c5597ba012191c196c25c9729664f47
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-03 19:11:26 +0530

    section sub title added

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,6 +8,15 @@ const About = () => {
         <h2 className="section__title">
           About <span>Me</span>
         </h2>
+
+        <div className="about__container container">
+          <div className="about__info">
+            <h3 className="section__subtitle">
+              Personal Info.
+            </h3>
+
+          </div>
+        </div>
       </section>
     </main>
   )
