2df04ed1a045156530c769c81b5fcf37dd2f7b62
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-08 01:02:40 +0530

    map projects

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { projects } from '../../data'
+import ProjectsItem from '../../components/ProjectsItem'
 
 const Project = () => {
   return (
-    <div>Project</div>
+    <section className="prjects section">
+      <h2 className="section__title">
+        My <span>Project</span>
+      </h2>
+
+      <div className="projects__container container rcontainer">
+        {projects.map((item) => {
+          return <ProjectsItem key={item.id}{...item}/>
+        })}
+      </div>
+    </section>
   )
 }
 
