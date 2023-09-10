07b91ca0fec670177c80c51263259e6215aa8cc5
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-11 00:59:57 +0530

    title and desc for contact us

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,4 +1,14 @@
 import React from 'react'
+import {
+  FaEnvelopeOpen, 
+  FaPhoneSquare,
+  FaLinkedinIn,
+  FaGithub,
+  FaDiscord  
+} from 'react-icons/fa'
+import {FaMedium} from 'react-icons/fa6'
+import {FiSend} from 'react-icons/fi'
+import './contact.css'
 
 const Contact = () => {
   return (
@@ -6,6 +16,22 @@ const Contact = () => {
       <h2 className="section__title">
         Get In <span>Touch</span>
       </h2>
+
+      <div className="contact__container rcontainer container">
+        <div className="contact__data">
+          <h3 className="contact__title">
+            Let's Connect
+          </h3>
+          <p className="contact__desc">
+            Thank you for visiting my portfolio! Whether it's internships, advice, 
+            or just a friendly chat, I'm all ears. I'm enthusiastic about learning, 
+            growing, and contributing. Reach out with your questions, collaboration ideas, 
+            or anything else. Your feedback is valuable, and I'm excited about the possibilities. 
+            Let's connect and achieve great things together.
+          </p>
+
+        </div>
+      </div>
     </section>
   )
 }
