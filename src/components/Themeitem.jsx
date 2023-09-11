d91dfd313f2eb08ba0af1ee146ad1de1e3da1fbe
Ravindu Kavinda <dlravindukavinda@gmail.com>
2023-09-11 19:18:05 +0530

    switcher toggle

--- src/components/Themeitem.jsx
+++ src/components/Themeitem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Themeitem = ({color, img}) => {
   return (
-    <img src={img} alt="" className='theme_img' />
+    <img src={img} alt="" className='theme__img' />
     )
 }
 
